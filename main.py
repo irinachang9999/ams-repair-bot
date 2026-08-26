52c9208e33616c5a1d92e83253d08d07394d267a
irinachang9999 <irina.chang@shopee.com>
2026-08-26 21:12:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,25 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def webhook():
+
+    # GET 驗證
+    challenge = request.args.get("seatalk_challenge")
+    if challenge:
+        return challenge
+
     if request.method == "GET":
-        challenge = request.args.get("seatalk_challenge")
-        if challenge:
-            return challenge
         return "Webhook Ready"
 
-    if request.method == "POST":
-        data = request.get_json(silent=True)
+    # POST 驗證
+    data = request.get_json(silent=True)
 
-        if data and "seatalk_challenge" in data:
-            return data["seatalk_challenge"]
+    if data and "seatalk_challenge" in data:
+        return data["seatalk_challenge"]
 
-        print(data)
-        return "OK"
+    print(data)
+
+    return "OK", 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
